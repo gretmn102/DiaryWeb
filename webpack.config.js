38555f4a6b92a623f3dfb837b8f005246baaf4ce
gretmn102 <erlgreywatson@gmail.com>
2023-01-18 08:33:24 +0200

    build: hot mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
+// @ts-check
+"use strict";
 const path = require('path');
 
 const isProduction = !process.argv.find(v => v.indexOf('watch') !== -1 || v.indexOf('serve') !== -1);
 const isDevelopment = !isProduction && process.env.NODE_ENV !== 'production';
 
+/** @type {import('webpack').Configuration} */
 module.exports = {
   entry: './src/Program.fs.js',
   mode: isProduction ? 'production' : 'development',
   devtool: isDevelopment ? 'inline-source-map' : undefined,
   devServer: {
     static: './public',
+    hot: true
   },
   output: {
     filename: '[name].bundle.js',
