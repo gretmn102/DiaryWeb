27f5bb9b5d34842e04e9b17ff7998bea72af1f20
gretmn102 <erlgreywatson@gmail.com>
2023-01-19 20:18:24 +0200

    add service worker

--- src/Program.fs
+++ src/Program.fs
@@ -2,6 +2,8 @@
 open Feliz
 open Browser.Dom
 open Fable.Builders.Fela
+open Fable.Core
+open Fable.Core
 
 let view =
     Fela.RendererProvider {
@@ -9,6 +11,14 @@ let view =
 
         Index.router ()
     }
+
+module RegisterServiceWorker =
+    type [<AllowNullLiteral>] IExports =
+        abstract ``default``: unit -> unit
+
+    let [<Import("*","./registerServiceWorker.js")>] registerServiceWorker: IExports = jsNative
+
+RegisterServiceWorker.registerServiceWorker.``default`` ()
 
 let root = ReactDOM.createRoot(document.getElementById "feliz-app")
 root.render(view)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 // @ts-check
 "use strict";
 const path = require('path');
+const { DefinePlugin } = require('webpack');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 const isProduction = !process.argv.find(v => v.indexOf('watch') !== -1 || v.indexOf('serve') !== -1);
 const isDevelopment = !isProduction && process.env.NODE_ENV !== 'production';
@@ -18,4 +20,14 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
+  plugins: [
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(isProduction && "production"),
+      'process.env.PUBLIC_URL': JSON.stringify('.'),
+    }),
+    isProduction ? new GenerateSW({
+      swDest: 'sw.js',
+      sourcemap: false
+    }) : (_ => undefined)
+  ]
 };
